8ab6a5831ea8085a013561e2046a0adcf2a9de84
Shubham Luthra <luthra2059@gmail.com>
2020-08-28 12:29:26 +0530

    Updates

---
+++ precache-manifest.33d8dcaa8a0246c1dc856d75006f20cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec94a629034a1796498a220a9839114f",
+    "url": "/Todo-React/index.html"
+  },
+  {
+    "revision": "094cd4ba4341e03e8074",
+    "url": "/Todo-React/static/css/main.bd6b84fb.chunk.css"
+  },
+  {
+    "revision": "fd89364cc571b32ed6c2",
+    "url": "/Todo-React/static/js/2.5eadb033.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-React/static/js/2.5eadb033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094cd4ba4341e03e8074",
+    "url": "/Todo-React/static/js/main.74afbbef.chunk.js"
+  },
+  {
+    "revision": "98284c2f565bfe545027",
+    "url": "/Todo-React/static/js/runtime-main.b014b2e6.js"
+  }
+]);
